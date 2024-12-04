604e27b193e0cc3db282339e2b3b71b362d14a34
Ahmed Ismail <108951693+ahmedismail1926@users.noreply.github.com>
2024-12-04 00:55:34 -0500

    update 12:55

--- TheGUI.py
+++ TheGUI.py
@@ -2,7 +2,8 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QStackedWidget, QVBoxLayout, QFormLayout,
     QLabel, QLineEdit, QPushButton, QMessageBox, QComboBox, QSpinBox
 )
-import database
+import database,heart_disease_clf,hypertension_clf,stroke_clf
+
 
 class LoginPage(QWidget):
     """Login page."""
@@ -113,7 +114,7 @@ class DiseaseChoicePage(QWidget):
         layout.addWidget(QLabel("Choose a disease to check:"))
 
         self.disease_combo = QComboBox(self)
-        self.disease_combo.addItems(["Heart Disease", "Disease 2", "Disease 3"])
+        self.disease_combo.addItems(["Heart Disease", "hypertension", "stroke"])
         layout.addWidget(self.disease_combo)
 
         next_button = QPushButton("Next", self)
@@ -126,6 +127,10 @@ class DiseaseChoicePage(QWidget):
         selected_disease = self.disease_combo.currentText()
         if selected_disease == "Heart Disease":
             self.parent.switch_to_heart_disease_form()
+        elif selected_disease == "hypertension":
+            self.parent.switch_to_hypertension_form()
+        elif selected_disease== "stroke":
+            self.parent.switch_to_stroke_form()
 
 
 class HeartDiseaseFormPage(QWidget):
@@ -208,9 +213,149 @@ class HeartDiseaseFormPage(QWidget):
             "obesity": self.obesity_input.currentText(),
             "blood_sugar": self.blood_sugar_input.value(),
         }
+        
         QMessageBox.information(self, "Success", "Attributes saved!")
         print(self.parent.heart_disease_data)
 
+class HypertensionFormPage(QWidget):
+    """Form page for Hypertension attributes."""
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QFormLayout()
+
+       
+        self.cp = QComboBox(self)
+        self.cp.addItems(["asymptomatic","typical angina","atypical angina","non-anginal"])
+        layout.addRow(QLabel("Chest pain:"), self.cp)
+
+        
+        self.trestbps = QSpinBox(self)
+        self.trestbps.setRange(80, 200)
+        layout.addRow(QLabel("resting blood pressure:"), self.trestbps)
+
+        
+        self.chol = QSpinBox(self)
+        self.chol.setRange(50, 200)
+        layout.addRow(QLabel("Chol level:"), self.chol)
+
+       
+        self.fbs = QComboBox(self)
+        self.fbs.addItems(['No','Yes'])
+        layout.addRow(QLabel("fasting blood sugar:"), self.fbs)
+
+        
+        self.restecg = QComboBox(self)
+        self.restecg.addItems(['No','Yes'])
+        layout.addRow(QLabel("Resting ECG"), self.restecg)
+
+        
+        self.thelach = QSpinBox(self)
+        self.thelach.setRange(80,200)
+        layout.addRow(QLabel("Minimum heart rate achieved"), self.thelach)
+        
+        self.exang = QComboBox(self)
+        self.exang.addItems(['No','Yes'])
+        layout.addRow(QLabel("Exercise induced angina:"), self.exang)
+        self.oldpeak = QSpinBox(self)
+        self.oldpeak.setRange(0,5)
+        layout.addRow(QLabel("ST depression induced by exercise relative to rest:"), self.oldpeak)
+
+        
+        self.slope = QComboBox(self)
+        self.slope.addItems(["Upsloping","Flat"," Downsloping"])
+        layout.addRow(QLabel(" peak exercise ST segment:"), self.slope)
+        
+        self.ca=QComboBox(self)
+        self.ca.addItems(['0','1','2','3'])
+        layout.addRow(QLabel("Calcium:"),self.ca)
+        
+        self.thal = QComboBox(self)
+        self.thal.addItems(['1','2','3'])
+        layout.addRow(QLabel("Blood Sugar Level:"), self.thal)
+
+        
+        save_button = QPushButton("Save", self)
+        save_button.clicked.connect(self.save_attributes)
+        layout.addWidget(save_button)
+
+        self.setLayout(layout)
+
+    def save_attributes(self):
+        self.parent.hypertension_data ={
+            "chest pain":self.cp.currentText(),
+            "trestbps":self.trestbps.value(),
+            "cholestrol level":self.chol.value(),
+            "fbs":self.fbs.currentText(),
+            "Resting ECG":self.restecg.currentText(),
+            "the lach":self.thelach.value(),
+            "exang":self.exang.currentText(),
+            "oldpeak":self.oldpeak.value(),
+            "slope":self.slope.currentText(),
+            "ca":self.ca.currentText(),
+            "thal":self.thal.currentText()
+        }
+        QMessageBox.information(self, "Success", "Attributes saved!")
+        print(self.parent.heart_disease_data)
+class StrokeFormPage(QWidget):
+    """Form page for Stroke attributes."""
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QFormLayout()
+
+        self.age_input = QSpinBox(self)
+        self.age_input.setRange(1, 120)
+        layout.addRow(QLabel("Age:"), self.age_input)
+
+        self.hypertension_input = QComboBox(self)
+        self.hypertension_input.addItems(["No", "Yes"])
+        layout.addRow(QLabel("Hypertension:"), self.hypertension_input)
+
+        self.heart_disease_input = QComboBox(self)
+        self.heart_disease_input.addItems(["No", "Yes"])
+        layout.addRow(QLabel("Heart Disease:"), self.heart_disease_input)
+
+        self.married_input = QComboBox(self)
+        self.married_input.addItems(["No", "Yes"])
+        layout.addRow(QLabel("Ever Married:"), self.married_input)
+
+        self.avg_glucose_input = QSpinBox(self)
+        self.avg_glucose_input.setRange(50, 400)
+        layout.addRow(QLabel("Average Glucose Level:"), self.avg_glucose_input)
+
+        self.bmi_input = QSpinBox(self)
+        self.bmi_input.setRange(10, 50)
+        layout.addRow(QLabel("BMI:"), self.bmi_input)
+
+        self.smoking_status_input = QComboBox(self)
+        self.smoking_status_input.addItems(["No", "Yes"])
+        layout.addRow(QLabel("Smoking Status:"), self.smoking_status_input)
+
+        save_button = QPushButton("Save", self)
+        save_button.clicked.connect(self.save_attributes)
+        layout.addWidget(save_button)
+
+        self.setLayout(layout)
+
+    def save_attributes(self):
+        self.parent.stroke_data = {
+            "age": self.age_input.value(),
+            "hypertension": self.hypertension_input.currentText(),
+            "heart_disease": self.heart_disease_input.currentText(),
+            "ever_married": self.married_input.currentText(),
+            "avg_glucose_level": self.avg_glucose_input.value(),
+            "bmi": self.bmi_input.value(),
+            "smoking_status": self.smoking_status_input.currentText(),
+        }
+        QMessageBox.information(self, "Success", "Attributes saved!")
+        print(self.parent.stroke_data)
 
 class MainApp(QStackedWidget):
     """Main application managing multiple pages."""
@@ -218,16 +363,21 @@ class MainApp(QStackedWidget):
         super().__init__()
         self.current_user_id = None
         self.heart_disease_data = {}
-
+        self.hypertension_data={}
+        self.stroke_data={}
         self.login_page = LoginPage(self)
         self.sign_up_page = SignUpPage(self)
         self.disease_choice_page = DiseaseChoicePage(self)
         self.heart_disease_form_page = HeartDiseaseFormPage(self)
+        self.hypertension_form_page=HypertensionFormPage(self)
+        self.stroke_form_page=StrokeFormPage(self)
 
         self.addWidget(self.login_page)
         self.addWidget(self.sign_up_page)
         self.addWidget(self.disease_choice_page)
         self.addWidget(self.heart_disease_form_page)
+        self.addWidget(self.hypertension_form_page)
+        self.addWidget(self.stroke_form_page)
 
         self.setCurrentWidget(self.login_page)
 
@@ -243,6 +393,10 @@ class MainApp(QStackedWidget):
     def switch_to_heart_disease_form(self):
         self.setCurrentWidget(self.heart_disease_form_page)
 
+    def switch_to_hypertension_form(self):
+        self.setCurrentWidget(self.hypertension_form_page)
+    def switch_to_stroke_form(self):
+        self.setCurrentWidget(self.stroke_form_page)
 
 if __name__ == "__main__":
     import sys
